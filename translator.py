0ba05a6f0e2f31f651d88748fb88aecdf2d96d14
Andrew Kazakin <andrew.kazakin@gmail.com>
2013-08-24 01:25:29 +0400

    Just output possible know.

--- translator.py
+++ translator.py
@@ -4,6 +4,8 @@ import inspect
 from collections import defaultdict, OrderedDict
 from datastructures import DefaultOrderedDict
 
+from argparse import ArgumentParser
+
 # Some ideas:
 # * routing stuff can be externalized (like Django's urls.py)
 # * access can be switched on/off (include/exclude/glob/...)
@@ -14,46 +16,6 @@ from datastructures import DefaultOrderedDict
 # Approach: it should be like request-response, i.e.
 #   I can init same structure for output and input/for validation
 # Is Django Forms is appropriate here? It's too complicated :)
-
-def get_pkg_modules(packages):
-    pkg_modules = defaultdict(list)
-    for imp, name, _ in pkgutil.iter_modules(packages):
-        pkg_name = os.path.basename(imp.path)
-        pkg_modules[pkg_name].append(name)
-    return pkg_modules
-
-
-def get_functions(pkg_modules):
-    functions = OrderedDict()
-    for pkg_name in pkg_modules:
-        for modname in pkg_modules[pkg_name]:
-            full_modname = '{0}.{1}'.format(pkg_name, modname)
-            pkg = __import__(full_modname)
-            mod = getattr(pkg, modname)
-            for name, function in inspect.getmembers(mod, inspect.isfunction):
-                full_name = '{0}.{1}'.format(full_modname, name)
-                functions[full_name] = function
-    return functions
-
-
-def print_fun_arguments(pkg_paths):
-    funs = get_functions(get_pkg_modules(pkg_paths))
-    for name, function in funs.items():
-        spec = inspect.getargspec(function)
-        args = []
-        kwargs = OrderedDict()
-        for i, arg in enumerate(spec.args):
-            z = i - len(spec.args)
-            if spec.defaults and len(spec.defaults) >= abs(z):
-                d = len(spec.args) - len(spec.defaults)
-                assert (i - d) >= 0, (i, d)
-                kwargs[arg] = spec.defaults[i - d]
-            else:
-                args.append(arg)
-        print name, ':', ', '.join(args), ', '.join('{0}={1}'.format(k,v) for k,v in kwargs.items())
-
-
-#---------------
 
 
 def functions(pkg_name, modname):
@@ -96,9 +58,17 @@ def print_fun_args(pkg_paths):
             for func in functions:
                 args = [sarg(n, d) for n, d in func['arguments'].items()]
                 print pkg_name, mod_name, func['name'], ','.join(args)
-#----------------------
 
-if __name__ == '__main__':
+
+def main():
+    parser = ArgumentParser(description="Exploses any function from any "
+            "module in 'scripts' dir to command line")
+    parser.add_argument('--module', dest='module')
+
     pkg_paths = ['./scripts',]
     #print_fun_arguments(pkg_paths)
     print_fun_args(pkg_paths)
+
+
+if __name__ == '__main__':
+    main()
